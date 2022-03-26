1bbea5e2e680ee1547c56b8e79867fc6ff1e1809
ssajiny <ssajiny@naver.com>
2022-03-26 23:27:40 +0900

    export complete

--- src/components/Excel.jsx
+++ src/components/Excel.jsx
@@ -2,37 +2,51 @@ import React, { useState } from 'react';
 import * as XLSX from 'xlsx';
 
 export const Excel = () => {   
-    const[fileName, setFileName] = useState(null);
-    const[tables, setTables] = useState();
+    const[tables, setTables] = useState([]);
 
-    const handleFile = async (e) => {
+    const handleFile = (file) => {
         
         const promise = new Promise((resolve, reject) => {
+            const fileReader = new FileReader();
+            fileReader.readAsArrayBuffer(file);
 
-        })
+            fileReader.onload =(e) => {
+                const bufferArray = e.target.result;
+                const workbook = XLSX.read(bufferArray, {type: 'buffer'});
+                const worksheetname = workbook.SheetNames[0];
+                const worksheet = workbook.Sheets[worksheetname];
+                const data = XLSX.utils.sheet_to_json(worksheet, {raw:false});
+                resolve(data);
+            };
 
-        const file = e.target.files[0];
-        setFileName(file.name);
-        const data = await file.arrayBuffer();
-        const workbook = XLSX.read(data);
-        const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-        const jsonData = XLSX.utils.sheet_to_json(worksheet, {
-            header: 1,
-            defval: "",
-            raw:false
+            fileReader.onerror = (error) => {
+                reject(error);
+            };
         });
 
+        promise.then((d) => {
+            setTables(d);
+        });
+    };
+
+    const handleOnExport = () => {
+        var workbook = XLSX.utils.book_new(),
+        worksheet = XLSX.utils.json_to_sheet(tables);
+        // worksheet = XLSX.utils.json_to_sheet(array);
+        // worksheet = XLSX.utils.aoa_to_sheet(tables);
+
+        XLSX.utils.book_append_sheet(workbook, worksheet, "test");
+        XLSX.writeFile(workbook, "test.xlsx");
     };
 
     return (  
         <div>
             <h1>Excel Practice</h1>
-            { fileName && (
-                 <p>
-                    FileName: <span>{ fileName }</span>
-                </p> )
-            }
-            <input type="file" onChange={(e) => handleFile(e) }/>
+            <input type="file" onChange={(e) => {
+                const file = e.target.files[0];
+                handleFile(file);
+            }}/>
+            <button onClick={handleOnExport}>Export</button>
             <table>
                 <thead>
                     <tr>
@@ -48,11 +62,19 @@ export const Excel = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                    </tr>
+                    { tables.map((table) => (
+                        <tr key={table.product_num}>
+                            <th> { table.wear_date } </th>
+                            <td>{ table.product_num }</td>
+                            <td>{ table.product_name }</td>
+                            <td>{ table.weight }</td>
+                            <td>{ table.thickness }</td>
+                            <td>{ table.width }</td>
+                            <td>{ table.length }</td>
+                            <td>{ table.company }</td>
+                            <td>{ table.manager }</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
